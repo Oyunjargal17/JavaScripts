4140f7c2725f6b8a189d738b05c23cce6359d526
25LP1983 25LP1983 <25LP1983@admins-iMac.local>
2025-11-17 19:56:58 +0800

    12.17 301 angid

--- bodlogo3.js
+++ bodlogo3.js
@@ -1,9 +1,102 @@
-//Bodlogo 1
+// //Bodlogo 1
 
-const tsag1 = 38;
-let day1 = tsag1 / 24;
-let hour1 = tsag1 % 24;
-console.log("Bodlogo 1 ");
-console.log(Math.floor(day1), "udur", hour1, "tsag");
+// const tsag1 = 38;
+// let day1 = tsag1 / 24;
+// let hour1 = tsag1 % 24;
+// console.log("Bodlogo 1 ");
+// console.log(Math.floor(day1), "udur", hour1, "tsag");
 
-//Bodlogo 2
+// //Bodlogo 2
+// const day2 = 4;
+// const hour2 = 40;
+// let tsagUdur2 = day2 * 24 + hour2;
+// console.log("Bodlog 2. Hariu ni= ", tsagUdur2, "tsag");
+
+// //Bodlogo 3
+// const mounts = 106;
+// let jil3 = mounts / 12;
+// let sar3 = mounts % 12;
+// console.log(
+//   "Bodlogo 3 hariu ni =",
+//   Math.floor(jil3),
+//   "jil",
+//   Math.floor(sar3),
+//   "sar bna."
+// );
+
+// //Bodlogo 4
+// const year4 = 90;
+// const mount4 = 98;
+// let jil4 = year4 * 12;
+// let sar4 = jil4 + mount4;
+// console.log("Bodlogo 4 hariu ni =", sar4, "sar bna.");
+
+// //Bodlogo 5
+// const b = 9;
+// const c = 3;
+// const d = 2;
+// const e = 8;
+// let min5 = b;
+// if (c < min5) {
+//   min5 = c;
+// }
+// if (d < min5) {
+//   min5 = d;
+// }
+// if (e < min5) {
+//   min5 = e;
+// }
+// console.log("Bodlogo 5 hariu ni =", min5);
+
+// //Bodlogo 6
+// const a1 = 2;
+// const b1 = 9;
+// const c1 = 4;
+// const d1 = 3;
+
+// let urjwer6 = 1;
+
+// if (a1 < 5) {
+//   urjwer6 = urjwer6 * a1;
+// }
+// if (b1 < 5) {
+//   urjwer6 = urjwer6 * b1;
+// }
+// if (c1 < 5) {
+//   urjwer6 = urjwer6 * c1;
+// }
+// if (d1 < 5) {
+//   urjwer6 = urjwer6 * d1;
+// }
+// console.log("Bodlogo 6 hariu ni =", urjwer6);
+
+// //Bodlogo 7
+// const a7 = 11;
+// const b7 = 9;
+// const c7 = 4;
+// const d7 = 3;
+// const e7 = 6;
+// const f7 = 2;
+
+// let urjwer7 = 1;
+
+// if (a7 > 5) {
+//   urjwer7 *= a7;
+// }
+// if (b7 > 5) {
+//   urjwer7 *= b7;
+// }
+// if (c7 > 5) {
+//   urjwer7 *= c7;
+// }
+// if (d7 > 5) {
+//   urjwer7 *= d7;
+// }
+// if (e7 > 5) {
+//   urjwer7 *= e7;
+// }
+// if (f7 > 5) {
+//   urjwer7 *= f7;
+// }
+
+// console.log("Bodlogo 7 hariu ni =", urjwer7);
