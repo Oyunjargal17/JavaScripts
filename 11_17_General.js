37d760539e656d95146bed22885663e8a02610ca
25lp1983 User <25lp1983@G301-CC0209.local>
2025-12-01 19:55:27 +0800

    12_01 301 angid

--- 11_17_General.js
+++ 11_17_General.js
@@ -1,13 +1,17 @@
 //1.  x = 5, y = 3. x + y-г console-д хэвлэ.
-// const x=5;
-// const y=3;
-// console.log(x+y);
+function bodlogo1() {
+  let a = prompt("x = 5, y = 3. x + y-г console-д хэвлэ.");
+  const x = 5;
+  const y = 3;
+  let h = x + y;
+  let t = "x = 5, y = 3. x + y-г console-д хэвлэ.";
+  document.getElementById("hariult").innerHTML = t + "<br>" + h;
+}
 
 //2.  a = 10, b = 4. a - b-г console-д хэвлэ.
 // const a=10;
 // const b=4;
 // console.log(a-b);
-
 
 //3. p = 7, q = 6. p * q-г хэвлэ.
 // const p=7;
@@ -24,30 +28,25 @@
 // const y=4;
 // console.log(x%y);
 
-
 //6.  num = 10. num = num + 5 гэж бичээд num-г хэвлэ.
 // let num=10;
 // num= num+5;
 // console.log(num);
-
 
 //7.  num = 20. num += 10 гэж бичээд num-г хэвлэ.
 // let num=20;
 // num +=10;
 // console.log(num);
 
-
 //8.  num = 50. num -= 20 гэж бичээд num-г хэвлэ.
 // let num=50;
 // num -=20;
 // console.log(num);
 
-
 //9.  num = 6. num *= 3 гэж бичээд num-г хэвлэ.
 // let num=6;
 // num *= 3;
 // console.log(num);
-
 
 //10. num = 16. num /= 4 гэж бичээд num-г хэвлэ.
 // let num=16;
--- 11_26_first_game.js
+++ 11_26_first_game.js
@@ -2,48 +2,79 @@
 // харьцуулж тэнцсэн тохиолдолд ялсан тэнцээгүй бол ялагдсан
 // гэж хэвлэх жижиг тоглоом хийнэ.
 // Мөн таны тэддэхь удаагийн оролдого гэснийг гаргаж ирнэ.
-const submitBtn = document.getElementById("submit-btn");
-const input = document.getElementById("input");
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-console.log(randomNumber);
-let counter = 0;
-function start() {
-  let value = 0;
-  counter++;
+// const submitBtn = document.getElementById("submit-btn");
+// const input = document.getElementById("input");
+// const randomNumber = Math.floor(Math.random() * 10 + 1);
+// console.log(randomNumber);
+// let counter = 0;
+// function start() {
+//   let value = 0;
+//   counter++;
+// ====================================================
+// console.log(randomNumber);
+// while (counter <= 5) {
+// value = prompt("Ta toogoo oruulna uu?");
 
-  // console.log(randomNumber);
-  // while (counter <= 5) {
-  // value = prompt("Ta toogoo oruulna uu?");
+//   const sulZai = value.trim();
+//   if (sulZai === "") {
+//     console.log("Ta hooson utga bolon zai awahgui baih");
+//     continue;
+//   }
 
-  //   const sulZai = value.trim();
-  //   if (sulZai === "") {
-  //     console.log("Ta hooson utga bolon zai awahgui baih");
-  //     continue;
-  //   }
+// const values = Number(value);
+// if (isNaN(values)) {
+//   console.log("Ta zowhon too oruulna uu?");
+//   continue;
+// }
+// if (value <= 0) {
+//   console.log("Ta surug utga oruulahgui baina uu?");
+//   continue;
+// }
 
-  // const values = Number(value);
-  // if (isNaN(values)) {
-  //   console.log("Ta zowhon too oruulna uu?");
-  //   continue;
-  // }
-  // if (value <= 0) {
-  //   console.log("Ta surug utga oruulahgui baina uu?");
-  //   continue;
-  // }
+// let udaa = counter++;
+// console.log(`tanii ${udaa}-iin oroldlogo:`);
+// ============================================================
+//   if (randomNumber == input.value) {
+//     text.innerHTML = "Баяр хүргэе 👏. Та хожлоо 🎉";
+//   } else if (randomNumber > input.value) {
+//     text.innerHTML = "Tanii oruulsan utgaas ih baina!";
+//   } else if (randomNumber < input.value) {
+//     text.innerHTML = "Tanii oruulsan utgaas baga baina!";
+//   }
+//   input.value = "";
+//   if (counter == 5) {
+//     text.innerHTML = "Tanii too oruulah oroldlogo duuslaa";
+//     submitBtn.disabled = true;
+//
+// }
+// }
+// const orolt = document.getElementById("orolt");
+// // let hariult = document.getElementById("hariult");
+// const submitBtn = document.getElementById("submit-btn");
+// const randomNumber = 1;
+// let counter = 0;
+// let or = [];
 
-  // let udaa = counter++;
-  // console.log(`tanii ${udaa}-iin oroldlogo:`);
-  if (randomNumber == input.value) {
-    text.innerHTML = "Баяр хүргэе 👏. Та хожлоо 🎉";
-  } else if (randomNumber > input.value) {
-    text.innerHTML = "Tanii oruulsan utgaas ih baina!";
-  } else if (randomNumber < input.value) {
-    text.innerHTML = "Tanii oruulsan utgaas baga baina!";
-  }
-  input.value = "";
-  if (counter == 5) {
-    text.innerHTML = "Tanii too oruulah oroldlogo duuslaa";
-    submitBtn.disabled = true;
-  }
-}
+// function start() {
+//   counter++;
+
+//   if (orolt.value > randomNumber) {
+//     hariult.innerHTML = "Tanii oruulsan utgaas baga too baina";
+//   }
+//   if (orolt.value < randomNumber) {
+//     hariult.innerHTML = "Tanii oruulsan utgaas ikh too baina";
+//   }
+
+//   if (counter == 5) {
+//     hariult.innerHTML =
+//       "Ta niit 5 udaagiin oroldlogo hiisen bna. Uunees tsaash gazar ugui";
+//     submitBtn.disabled = true;
+//   }
+//   if (randomNumber == orolt.value) {
+//     hariult.innerHTML = "Bayr hurgey! Ta hojloo";
+//   }
+//   console.log(counter);
+//   or.push(orolt.value);
+//   console.log(or);
+//   orolt.value = "";
 // }
