d01167f6bb27fcc6af20d67ec40fd14e783d2ff1
25lp1983 User <25lp1983@G301-CC0209.local>
2025-12-03 20:15:22 +0800

    12_03 301 anigd

--- 12_03_Homework.js
+++ 12_03_Homework.js
@@ -1,44 +1,222 @@
 // filter+reduce+map бодлого
 // 1. Тэгш тоонуудын квадратын нийлбэр
 // [1,2,3,4,5,6] → 2²+4²+6² = 56
-const arr = [1, 2, 3, 4, 5, 6];
-const oddEven= arr.map((num)=>)
+// const arr = [1, 2, 3, 4, 5, 6];
+// const oddEven = arr.filter((numOddEven) => {
+//   if (numOddEven % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+// console.log(oddEven);
+// const cube = oddEven.map((numCube) => {
+//   return numCube * numCube;
+// });
+// console.log(cube);
 
 // 2. 5-аас их тоонуудыг 3-аар үржүүлж нийлбэр
 // [2,6,4,10] → 63 + 103 = 48
+// const arr = [2, 6, 4, 10];
+// const maxFive = arr.filter((num) => {
+//   if (num > 5) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+// console.log(maxFive);
+// const multiplication = maxFive.map((num) => {
+//   return num * 3;
+// });
+// console.log(multiplication);
+// let sum = multiplication.reduce((accum, currentValue) => {
+//   return accum + currentValue;
+// }, 0);
+// console.log(sum);
+
 // 3. 3-аар хуваагдах тоонуудын квадратын нийлбэр
 // [3,4,6,7,9] → 3²+6²+9² = 126
+// const arr = [3, 4, 6, 7, 9];
+// const divided = arr.filter((num) => num % 3 === 0);
+// console.log(divided);
+// const cube = divided.map((num) => num * num);
+// console.log(cube);
+// const sum = cube.reduce((acum, currentValue) => acum + currentValue);
+// console.log(sum);
+
 // 4. 10-аас бага тоонуудын кубын нийлбэр
-// [2,5,12,7] → 2³+5³+7³ = 470
+// [2,5,12,7] → 2³+5³+7³ = 476
+// const arr = [2, 5, 12, 7];
+// const minTen = arr.filter((num) => num < 10);
+// console.log(minTen);
+// const cube = minTen.map((num) => num * num * num);
+// console.log(cube);
+// const sum = cube.reduce((accum, currentValue) => accum + currentValue);
+// console.log(sum);
+
 // 5. 0-с их тоонуудыг 2 дахин үржүүлж нийлбэр
 // [-2,3,0,5,-1] → (32 + 52) = 16
+// const arr = [-2, 3, 0, 5, -1];
+// const multiplication = arr.filter((num) => num > 0);
+// console.log(multiplication);
+// const cube = multiplication.map((num) => num * 2);
+// console.log(cube);
+// const sum = cube.reduce((accum, currentValue) => accum + currentValue);
+// console.log(sum);
+
 // 6. 2-аар хуваагдах тоонуудын дундаж
 // [2,5,8,10] → (2+8+10)/3 = 6.666…
+// const arr = [2, 5, 8, 10];
+// const avarge = arr.filter((num) => num % 2 === 0);
+// console.log(avarge);
+// const sum = avarge.reduce((accum, currentValue) => accum + currentValue);
+// console.log(sum);
+// const avargeResult = sum / avarge.length;
+// console.log(avargeResult);
+
 // 7. 3-аар хуваагдах тоонуудын дундаж
 // [3,4,6,7,9] → (3+6+9)/3 = 6
+// const arr = [3, 4, 6, 7, 9];
+// const avarge = arr.filter((num) => num % 3 === 0);
+// console.log(avarge);
+// const sum = avarge.reduce((accum, currentValue) => accum + currentValue);
+// console.log(sum);
+// const avargeResult = sum / avarge.length;
+// console.log(avargeResult);
+
 // 8. 5-аас их тоонуудыг модуль 2 үр дүнгээр шүүх, нийлбэр олох
 // [2,6,4,10,7] → 6+4+10 = 20
+// const arr = [2, 6, 4, 10, 7];
+// const maxFive = arr.filter((num) => num > 5);
+// console.log(maxFive);
+// const divided = maxFive.filter((num) => num % 2 === 0);
+// console.log(divided);
+// const sum = divided.reduce((accum, currentValue) => accum + currentValue);
+// console.log(sum);
+
 // 9. 3-аас бага тоонуудын үржвэр
 // [1,2,3,4,5] → 1*2 = 2
+// const arr = [1, 2, 3, 4, 5];
+// const min = arr.filter((num) => num < 3);
+// console.log(min);
+// const multiplication = min.reduce(
+//   (accum, currentValue) => accum * currentValue
+// );
+// console.log(multiplication);
+
 // 10. 5-аас бага тоонуудын квадратын нийлбэр
 // [1,2,3,6,7] → 1²+2²+3² = 14
+// const arr = [1, 2, 3, 6, 7];
+// const minFive = arr.filter((num) => num < 5);
+// console.log(minFive);
+// const multiplication = minFive.map((num) => num * num);
+// console.log(multiplication);
+// const sum = multiplication.reduce(
+//   (accum, currentValue) => accum + currentValue
+// );
+// console.log(sum);
+
 // 11. 10-аас их тоонуудын квадратын нийлбэр
 // [5,12,8,20,3] → 12² + 20² = 544
+// const arr = [5, 12, 8, 20, 3];
+// const max = arr.filter((num) => num > 10);
+// console.log(max);
+// const multiplication = max.map((num) => num * num);
+// console.log(multiplication);
+// const sum = multiplication.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 12. Тэгш тоонуудын кубын нийлбэр
 // [1,2,3,4,5,6] → 2³+4³+6³ = 288
+// const arr = [1, 2, 3, 4, 5, 6];
+// const oddEven = arr.filter((num) => num % 2 === 0);
+// console.log(oddEven);
+// const cube = oddEven.map((num) => num * num * num);
+// console.log(cube);
+// const sum = cube.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 13. 3-аар хуваагдах тоонуудын кубын нийлбэр
 // [3,4,6,7,9] → 3³+6³+9³ = 972
+// const arr = [3, 4, 6, 7, 9];
+// const divided = arr.filter((num) => num % 3 === 0);
+// console.log(divided);
+// const cube = divided.map((num) => num * num * num);
+// console.log(cube);
+// const sum = cube.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 14. 0-с их тоонуудын квадратын нийлбэр
 // [-5,0,4,7,-1] → 4²+7² = 65
+// const arr = [-5, 0, 4, 7, -1];
+// const max = arr.filter((num) => num > 0);
+// console.log(max);
+// const cube = max.map((num) => num * num);
+// console.log(cube);
+// const sum = cube.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 15. 10-аас их тоонуудыг 2 дахин үржүүлж нийлбэр
 // [8,12,15,5] → 122 + 152 = 54
+// const arr = [8, 12, 15, 5];
+// const max = arr.filter((num) => num > 10);
+// console.log(max);
+// const multiplication = max.map((num) => num * num);
+// console.log(multiplication);
+// const sum = multiplication.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 16. 5-аас их тоонуудын модуль 3 үр дүнг нийлүүлэх
 // [2,6,4,10,7] → 6%3 + 10%3 = 0+1 = 1
+// const arr = [2, 6, 4, 10, 7];
+// const max = arr.filter((num) => num > 5);
+// console.log(max);
+// const divided = max.map((num) => num % 3);
+// console.log(divided);
+// const sum = divided.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 17. 5-аас бага тоонуудын модуль 2 үр дүнг нийлүүлэх
 // [1,2,3,6,7] → 1%2 + 2%2 + 3%2 = 1+0+1 = 2
+// const arr = [1, 2, 3, 6, 7];
+// const min = arr.filter((num) => num < 5);
+// console.log(min);
+// const divided = min.map((num) => num % 2);
+// console.log(divided);
+// const sum = divided.reduce((accum, current) => accum + current);
+// console.log(sum);
+
 // 18. 3-аар хуваагдах тоонуудын квадратын нийлбэрийг мод 10
 // [3,4,6,7,9] → (3²+6²+9²)%10 = 126%10 = 6
+// const arr = [3, 4, 6, 7, 9];
+// const divided = arr.filter((num) => num % 3 === 0);
+// console.log(divided);
+// const multiplication = divided.map((num) => num * num);
+// console.log(multiplication);
+// const sum = multiplication.reduce((accum, current) => accum + current);
+// console.log(sum);
+// const dividedTen = sum % 10;
+// console.log(dividedTen);
+
 // 19. 10-аас их тоонуудын квадратын нийлбэрийг мод 7
 // [5,12,8,20,3] → (12² + 20²)%7 = 544%7 = 5
+// const arr = [5, 12, 8, 20, 3];
+// const max = arr.filter((num) => num > 10);
+// console.log(max);
+// const multiplication = max.map((num) => num * num);
+// console.log(multiplication);
+// const sum = multiplication.reduce((accum, current) => accum + current);
+// console.log(sum);
+// const dividedSeven = sum % 7;
+// console.log(dividedSeven);
+
 // 20. String массив: 5 үсгээс урт үгсийг filter → map → reduce ашиглан нийт урт
 // ["hi","hello","hey","world","javascript"] → hello+world+javascript = 5+5+10=20
+// const str = ["hi", "hello", "hey", "world", "javascript"];
+// const max = str.filter((word) => word.length >= 5);
+// console.log(max);
+// const current = max.map((word) => word.length);
+// console.log(current);
+// const sum = current.reduce((accum, curr) => accum + curr);
+// console.log(sum);
--- 12_03_filter.js
+++ 12_03_filter.js
@@ -70,17 +70,129 @@
 // 10. Жагсаалтаас давхардсан үгсийг авах (тэнцүү эсэхээр шалгах)["a","b","a","c","b"] → "a", "b" давхардсан учир → ["a","b"]
 // const chars = ["a", "b", "a", "c", "b"];
 // const temp = [];
-// const duplicate = chars.;
-// let i = 0;
-// function yesNo(letter) {
-//   // for(let i=0; i<str.length; i++)
-//   // {
-//   //     temp.push(letter[i]);
-//   //     if(letter[i])
-//   // }
-//   temp.push(letter[i]);
-//   if(temp=)
-//   console.log(temp);
+// const duplicate = chars.filter((char) => {
+//   if (!temp.includes(char)) {
+//     temp.push(char);
+//     return false;
+//   } else {
+//     return true;
+//   }
+// });
+// console.log(duplicate);
+
+//Бодлого1 fruits = ["apple", "banana", "kiwi", "grape"] дотор "kiwi" байгаа эсэхийг шалгаад, үр дүнг hasKiwi хувьсагчид оноогоорой.
+// const fruits = ["apple", "banana", "kiwi", "grape"];
+// const hasKiwi = fruits.includes("kiwi");
+// console.log(hasKiwi);
+
+//fruits = ["apple", "banana", "kiwi", "grape"] массив дотор "orange"
+// байгаа эсэхийг шалгаад, үр дүнг hasOrange хувьсагчид оноогоорой.
+
+// const fruits = ["apple", "banana", "kiwi", "grape"];
+// const hasOrange = fruits.includes("orange");
+// console.log(hasOrange);
+
+// members = ["Tuguldur", "Erdene", "Oyun", "Bat", "Namuun"]; массив дотор "Bat" нэртэй гишүүн байгаа бол, консолд
+// "Багийн гишүүн Бат байна." гэж хэвлэх кодыг $includes()$-ийг ашиглан бичээрэй.
+// const members = ["Tuguldur", "Erdene", "Oyun", "Bat", "Namuun"];
+// if (members.includes("Bat")) {
+//   console.log("Багийн гишүүн Бат байна.");
 // }
-// const newArr = str.filter(yesNo);
-// // console.log(newArr);
+
+// const shoppingCart = ["Mouse", "Monitor", "Webcam", "Bag"];  массив доторх бүтээгдэхүүнүүдээс
+//const availableProducts = ["Laptop", "Mouse", "Keyboard", "Monitor"]; массив дотор байгаа бүтээгдэхүүнүүдийг
+//filter() болон includes()-ийг ашиглан шүүж гаргаарай. Үр дүнг inStockItems хувьсагчид оноогоорой.
+// const shoppingCart = ["Mouse", "Monitor", "Webcam", "Bag"];
+// const availableProducts = ["Laptop", "Mouse", "Keyboard", "Monitor"];
+// const inStockItems = shoppingCart.filter((item) =>
+//   availableProducts.includes(item)
+// );
+// console.log(inStockItems);
+
+//const allStudents = ["Anu", "Bataa", "Tsolmon", "Delger", "Enkhtuya"]; массив доторх сурагчдаас
+//const passedStudents = ["Anu", "Tsolmon", "Enkhtuya"]; массив дотор байхгүй (өөрөөр хэлбэл, шалгалтад унасан) сурагчдыг
+//filter() болон includes() -ийг ашиглан шүүж гаргаарай.
+// Үр дүнг failedStudents хувьсагчид оноогоорой.
+// const allStudents = ["Anu", "Bataa", "Tsolmon", "Delger", "Enkhtuya"];
+// const passedStudents = ["Anu", "Tsolmon", "Enkhtuya"];
+// const failedStudents = allStudents.filter(
+//   (item) => !passedStudents.includes(item)
+// );
+// console.log(failedStudents);
+
+//let colors = ["red", "green", "blue"];
+// массив руу "yellow" гэсэн өнгийг push()-ийг ашиглан нэмээд,
+// colors массивыг хэвлэж үр дүнг шалгаарай.
+// let colors = ["red", "green", "blue"];
+// colors.push("yellow");
+// console.log(colors);
+
+//let colors = ["red", "green", "blue"]; массив руу "purple" болон "pink" гэсэн өнгөнүүдийг нэг дор
+// push()-ийг ашиглан нэмээд, colors массивыг хэвлэж үр дүнг шалгаарай.
+
+// let colors = ["red", "green", "blue"];
+// colors.push("purple", "pink");
+// console.log(colors);
+
+// let numbers = [1, 2, 3, 4, 5];
+// let doubled = []; // Энэ массив руу push хийнэ.
+//numbers массив доторх тоо тус бүрийг 2-оор үржүүлээд,
+// үр дүнг нь doubled массив руу for\ of давталт болон push()-ийг ашиглан нэмээд,
+// doubled массивыг хэвлэж үр дүнг шалгаарай.
+// let numbers = [1, 2, 3, 4, 5];
+// let doubled = []; // Энэ массив руу push хийнэ.
+// for (let i = 0; i < numbers.length; i++) {
+//   const result = numbers[i] * 2;
+//   doubled.push(result);
+// }
+// console.log(doubled);
+
+// const initialArr = [10, 25, 8, 40, 15];
+// const largeNumbers = []; // Энэ массив руу push хийнэ.
+//initialArr массив доторх 30-аас их тоонуудыг for\ of давталт, if нөхцөл,
+// болон push() -ийг ашиглан largeNumbers массив руу нэмээд,
+// largeNumbers массивыг хэвлэж үр дүнг шалгаарай.
+// const initialArr = [10, 25, 8, 40, 15, 12, 33, 49, 32];
+// const largeNumbers = []; // Энэ массив руу push хийнэ.
+// for (let i = 0; i < initialArr.length; i++) {
+//   const num = initialArr[i];
+//   if (num > 30) {
+//     largeNumbers.push(num);
+//   }
+// }
+// console.log(largeNumbers);
+// -------------------------------------------------------------
+// const initialArr = [10, 25, 8, 40, 15, 12, 33, 49, 32];
+// const largeNumbers = []; // Энэ массив руу push хийнэ.
+// for (const num of initialArr) {
+//   if (num > 30) {
+//     largeNumbers.push(num);
+//   }
+// }
+// console.log(largeNumbers);
+
+//const numbers = [1, -5, 10, -3, 8, -20];
+//const positiveNumbers = []; // Энэ массив руу push хийнэ.
+//numbers массив доторх тоо тус бүрийг давтаж,
+// сөрөг тоо (-5, -3, -20) байвал эерэг (absolute) утга болгоод,
+// positiveNumbers массив руу push() хийгээрэй. Харин эерэг тоо байвал (1, 10, 8)
+// өөрчлөхгүйгээр шууд push() хийнэ.
+// const numbers = [1, -5, 10, -3, 8, -20];
+// const positiveNumbers = []; // Энэ массив руу push хийнэ.
+// for (let num of numbers) {
+//   if (num < 0) {
+//     num = num * -1;
+//     positiveNumbers.push(num);
+//   } else {
+//     positiveNumbers.push(num);
+//   }
+// }
+// console.log(positiveNumbers);
+
+// --------------------------------------------------
+const numbers = [1, -5, 10, -3, 8, -20];
+const positiveNumbers = []; // Энэ массив руу push хийнэ.
+for (const num of numbers) {
+  positiveNumbers.push(Math.abs(num));
+}
+console.log(positiveNumbers);
