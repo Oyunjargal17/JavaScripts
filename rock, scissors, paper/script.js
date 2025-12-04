2d2808ad5833ab8a53b9caeda770118af6575af0
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2025-12-04 20:45:14 +0800

    12_04 angid

--- rock, scissors, paper/script.js
+++ rock, scissors, paper/script.js
@@ -1,19 +1,20 @@
-const rock = document.getElementById("rock");
-const scissors = document.getElementById("scissors");
-const paper = document.getElementById("paper");
+const rockButton = document.getElementById("main-rock");
+const paperButton = document.getElementById("main-paper");
+const scissorsButton = document.getElementById("main-scissors");
 const botScore = document.getElementById("bot-score");
 const userScore = document.getElementById("user-score");
 const userWinWin = document.getElementById("user-win");
 const botWinWin = document.getElementById("bot-win");
 const resetBtn = document.getElementById("reset");
+const vsImage = document.querySelector(".center img");
 
 let humanScore = 0;
 let computerScore = 0;
 const turn = (choice) => {
-  console.log(choice);
+  //   console.log(choice);
 
   const computerChoice = computerChoicer();
-  console.log(choice, computerChoice);
+  //   console.log(choice, computerChoice);
   if (choice === "rock" && computerChoice === "scissors") {
     // console.log("user Win");
     humanScore++;
@@ -47,31 +48,37 @@ const turn = (choice) => {
   console.log(computerScore);
   console.log(humanScore);
   if (computerScore === 5) {
-    botWinWin.innerHTML = "Game over: Computer Win";
-    reset.style.display = "block";
-    rock.disabled = true;
-    scissors.disabled = true;
-    paper.disabled = true;
+    botWinWin.innerHTML = "COMPUTER WON! 🎉";
+    resetBtn.style.display = "block";
+    vsImage.style.display = "none";
+    rockButton.disabled = true;
+    scissorsButton.disabled = true;
+    paperButton.disabled = true;
   }
   if (humanScore === 5) {
-    userWinWin.innerHTML = "Game over: Human Win";
-    reset.style.display = "block";
-    rock.disabled = true;
-    scissors.disabled = true;
-    paper.disabled = true;
+    userWinWin.innerHTML = "YOU WON! 🎉 ";
+    resetBtn.style.display = "block";
+    vsImage.style.display = "none";
+    rockButton.disabled = true;
+    scissorsButton.disabled = true;
+    paperButton.disabled = true;
   }
 };
 const resetGame = () => {
   humanScore = 0;
   computerScore = 0;
-  reset.style.display = "none";
+  userWinWin.innerHTML = "";
+  botWinWin.innerHTML = "";
+  resetBtn.style.display = "none";
   userScore.innerHTML = humanScore;
   botScore.innerHTML = computerScore;
-  rock.disabled = false;
-  paper.disabled = false;
-  scissors.disabled = false;
+  vsImage.style.display = "block";
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
   //   console.log(humanScore);
 };
+window.resetGame = resetGame;
 const computerChoicer = () => {
   const ramdomChoiceIndex = Math.floor(Math.random() * 3);
 
